6d3a86212027e32709f3ecb465eb8a3b294292e0
MdKaioumislam5 <afrankaiyum34@gmail.com>
2022-04-03 18:07:08 +0600

    all code updated

--- basic.js
+++ basic.js
@@ -1,8 +1,8 @@
 // problem no:1
 function productComponent(computer, watch, coffee) {
-    if(typeof (computer,watch,coffee) !='number'){
-        return'please enter a valid Product number';
-    }
+  if (typeof (computer, watch, coffee) != "number") {
+    return "please enter a valid Product number";
+  }
   let percomputer = 1000;
   let perwatch = 260;
   let percoffee = 50;
@@ -22,47 +22,73 @@ let totalProductPrice = productComponent(2, 2, 2);
 // problem no:2
 var products = [
   {
-      id: 1,
-      title: "laptop",
-      price: 567,
-      description: "Laptop Lorem ipsum dolor sit amet consectetur adipiscing elit, magna etiam fermentum accumsan rhoncus ullamcorper"
+    id: 1,
+    title: "laptop",
+    price: 567,
+    description:
+      "Laptop Lorem ipsum dolor sit amet consectetur adipiscing elit, magna etiam fermentum accumsan rhoncus ullamcorper",
   },
   {
-      id: 2,
-      title: "Mobile",
-      price: 367,
-      description: "Mobile Lorem ipsum dolor sit amet consectetur adipiscing elit, magna etiam fermentum accumsan rhoncus ullamcorper"
+    id: 2,
+    title: "Mobile",
+    price: 367,
+    description:
+      "Mobile Lorem ipsum dolor sit amet consectetur adipiscing elit, magna etiam fermentum accumsan rhoncus ullamcorper",
   },
   {
-      id: 3,
-      title: "Watch",
-      price: 180,
-      description: "Watch Lorem ipsum dolor sit amet consectetur adipiscing elit, magna etiam fermentum accumsan rhoncus ullamcorper"
+    id: 3,
+    title: "Watch",
+    price: 180,
+    description:
+      "Watch Lorem ipsum dolor sit amet consectetur adipiscing elit, magna etiam fermentum accumsan rhoncus ullamcorper",
   },
   {
-      id: 4,
-      title: "Coffee",
-      price: 40,
-      description: "Coffee Lorem ipsum dolor sit amet consectetur adipiscing elit, magna etiam fermentum accumsan rhoncus ullamcorper"
+    id: 4,
+    title: "Coffee",
+    price: 40,
+    description:
+      "Coffee Lorem ipsum dolor sit amet consectetur adipiscing elit, magna etiam fermentum accumsan rhoncus ullamcorper",
   },
-
 ];
-if(typeof (products) =='-'){
-  'please enter a valid Product number';
-}
-// console.log(products.pandaCost);
+// if (products.some((product) => product.title === "laptop")) {
+//   return'Object found inside the array';
+// } 
+// else {
+//   return'Object not found"';
+// }
 
+console.log(products[0].title);
+// var __FOUND = products.findIndex(function(product, index) {
+// 	if(product.title ==  'laptop')
+// 		return true;
+//     else{
+//       return'Product Not Found ';
+//     }
+// });
+// console.log(__FOUND[0]);
+// console.log(products.pandaCost);
 
 // problem no: 3
 function isLeapYear(year) {
-  if(year % 4 == 0) {
+  // leap year if perfectly divisible by 400
+  if (year % 400 == 0) {
     return 366 * 10;
   }
-  else{
+  // not a leap year if divisible by 100
+  // but not divisible by 400
+  else if (year % 100 == 0) {
+    return 365 * 10;
+  }
+  // leap year if not divisible by 100
+  // but divisible by 4
+  else if (year % 4 == 0) {
+    return 366 * 10;
+  }
+  // all other years are not leap years
+  else {
     return 365 * 10;
   }
 }
 
 const myYearMoney = isLeapYear(2022);
-console.log(myYearMoney);
-
+// console.log(myYearMoney);
